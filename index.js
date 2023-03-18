e632d7c54e3402781f0b648520147c2a7b5573ea
Anthony Lotson <focus_and_finish@yahoo.com>
2023-03-18 16:40:31 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+//const multiply = (62) 
+const multiply = (num1 * num2);
+
+const random = Math.ceil(Math.random());
+//Math.ceil (random);
+
+let num3 = 5;
+let num4 = 19;
+const mod = (num4 % num3);
+
+
+//let max = Math.max(5, 10, 15, 20) or
+
+let num5 = 20
+let max = Math.max((num1 - 20),num2, num3, num4, num5)
